1a4bf2e68b64285d5768def72539a67783df5892
shukurovfaxriddin <s91993846@gmail.com>
2025-05-15 16:31:40 +0500

    Update string.py

--- lesson-1/homework/string.py
+++ lesson-1/homework/string.py
@@ -1,1 +1,26 @@
+##_1
+a=int(input('kvadrat tomoni :' ))
 
+print('kvadratni uzunligi :',a*4)
+print('kvadratni yuzasi :', a**2)
+
+##_2
+b=int(input('doirani diametri :' ))
+
+print('doirani uzunligi;', b*3.14)
+
+##3
+a=int(input('birnchi son :' ))
+b=int(input('ikkinchi son :' ))
+
+print('orta arihmetigi',(a+b)/2)
+
+
+##4
+a=int(input('birnchi son :' ))
+b=int(input('ikkinchi son :' ))
+
+print('yigindisi',a+b)
+print('kopaytmasi',(a*b))
+print('birinchi son kvadrati',(a**2))
+print('ikkinchi son kvadrati',(b**2))
